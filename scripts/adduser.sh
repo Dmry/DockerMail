cb596a69cb8192095c39aec7e8343082faf5c9d5
Daniël Emmery <daniel@dmry.nl>
2019-10-15 22:31:25 +0200

    Now correctly asks for a password

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -6,4 +6,4 @@ docker exec -it dockermail_dovecot_1 doveadm pw -s BLF-CRYPT
 read -p "Please copy the hash above, excluding {BLF-CRYPT}: " hash
 
 echo "Please enter mysql root password next."
-docker exec -it dockermail_database_1 mysql -u root -psecret mailserver -e "INSERT INTO virtual_users (domain_id, password , email) VALUES ('1', '$hash', '$user');"
+docker exec -it dockermail_database_1 mysql -u root -p mailserver -e "INSERT INTO virtual_users (domain_id, password , email) VALUES ('1', '$hash', '$user');"
