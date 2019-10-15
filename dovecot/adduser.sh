c60bc336685e276f019412ad9f032dda28f00b15
Daniël Emmery <daniel@dmry.nl>
2019-10-15 20:01:19 +0200

    syntax error

--- dovecot/adduser.sh
+++ dovecot/adduser.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 read "Please enter a user (suggested format: user@maildomain.com)" user
 
-hash = doveadm pw -s BLF-CRYPT
+hash='doveadm pw -s BLF-CRYPT'
 mysql -u root -p somedb -e "mysql> INSERT INTO `mailserver`.`virtual_users`
   (`id`, `domain_id`, `password` , `email`)
 VALUES
