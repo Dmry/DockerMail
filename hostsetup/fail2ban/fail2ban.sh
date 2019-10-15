adbb7ac6fe070da69a4d73246712a212572fa202
Daniël Emmery <daniel@dmry.nl>
2019-10-15 22:03:33 +0200

    Fixes for new directory

--- hostsetup/fail2ban/fail2ban.sh
+++ hostsetup/fail2ban/fail2ban.sh
@@ -2,17 +2,15 @@
 
 apt install -y fail2ban
 
-hostfiles="../hostsetup/fail2ban"
-
 containers=(dovecot postfix)
 
 for item in ${array[*]}
 do
-    cp $hostfiles/fail2ban-$item-action.conf /etc/fail2ban/action.d/
-    cp $hostfiles/fail2ban-$item-filter.conf /etc/fail2ban/filter.d/
+    cp fail2ban-$item-action.conf /etc/fail2ban/action.d/
+    cp fail2ban-$item-filter.conf /etc/fail2ban/filter.d/
 done
 
-cp $hostfiles/jail.local /etc/fail2ban/
+cp jail.local /etc/fail2ban/
 
 echo "ssh port?"
 
