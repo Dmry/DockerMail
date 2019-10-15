9b23d837040b9391ffdcfbb94c061e22b3d17b5f
Daniël Emmery <daniel@dmry.nl>
2019-10-15 20:44:10 +0200

    Add user script

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 read -p "Please enter an e-mail adress for your new user: " user
 
-hash=docker exec -it dockermail_dovecot_1 doveadm pw -s BLF-CRYPT
+docker exec -it dockermail_dovecot_1 doveadm pw -s BLF-CRYPT
+
+read -p "Please copy the hash above, excluding {BLF-CRYPT}: " hash
+
+echo "Please enter mysql root password next."
 docker exec -it dockermail_database_1 mysql -u root -psecret mailserver -e "INSERT INTO virtual_users (domain_id, password , email) VALUES ('1', '$hash', '$user');"
