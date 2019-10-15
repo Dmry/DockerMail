2c3bb9ce5280b620487066201de708a7a3743c22
Daniël Emmery <daniel@dmry.nl>
2019-10-15 14:35:36 +0200

    Fail2Ban setup

---
+++ host setup/fail2ban/fail2ban.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+apt install -y fail2ban
+
+hostfiles = "../hostfiles"
+
+containers=(dovecot postfix)
+
+for item in ${array[*]}
+do
+    cp $hostfiles/fail2ban-$item-action.conf /etc/fail2ban/action.d/
+    cp $hostfiles/fail2ban-$item-filter.conf /etc/fail2ban/filter.d/
+done
+
+cp $hostfiles/jail.local /etc/fail2ban/
+
+echo "ssh port?\n"
+
+read ssh
+
+sed -i 's/port = ssh/port = $ssh/g' /etc/fail2ban/jail.local
